811df0382e9a60e978af71a8aeef5e943ca77db5
Alaa Ahmed <alaaa.ahmed@mail.utoronto.ca>
2017-09-23 15:05:15 -0400

    all files

--- PlayerAI.py
+++ PlayerAI.py
@@ -14,7 +14,7 @@ class PlayerAI:
     def do_move(self, world, friendly_units, enemy_units):
         """
         This method will get called every turn.
-        
+
         :param world: World object reflecting current game state
         :param friendly_units: list of FriendlyUnit objects
         :param enemy_units: list of EnemyUnit objects
@@ -28,3 +28,23 @@ class PlayerAI:
                                            world.get_closest_capturable_tile_from(unit.position, None).position,
                                            None)
             if path: world.move(unit, path[0])
+
+        # every firefly attacks enemy if one tile away
+
+
+        # first 8
+        # 1) first 2 will get closest tile
+        # 2) third will build a nest 5 tiles away
+        # 3) fourth will attack enemies nests
+        # 4) fifth attack weakest enemies
+        # 6) sixth get closest neutral tile
+        # 7) seventh attacks enemy nests
+        # 8) attack enemy tiles
+
+        # more than 8
+        # check that all 8 are still alive, otherwise send replacement
+        # stock up on all 4 adjacent tiles around nest
+            
+
+
+
